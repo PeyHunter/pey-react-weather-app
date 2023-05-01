f7332234a567d3e9e530c209eddf1d9af94e5526
PeyHunter <Nando.goat@gmail.com>
2023-05-01 16:47:39 +0200

    Creating different files and importing to head

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import { func } from "prop-types";
+import React from "react"
+import { createRoot } from "react-dom/client";
+import "./App.css"
+
+function weatherApp() {
+    return ("hello World");
+}
