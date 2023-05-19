c2ec137ef14e7511d901bb094a73000a1082e7da
PeyHunter <Nando.goat@gmail.com>
2023-05-19 16:28:41 +0200

    Setting up initial structure

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import axios from "axios"
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+        <h1>
+          Hello world
+        </h1>
+      
+      axios.get(url).then(displayTemp)
+      </header>
+    </div>
+  );
+}
+
+export default App;
