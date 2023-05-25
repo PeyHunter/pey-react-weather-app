e169be13ffc59aa6f0f9b9ec762c5052b737f710
PeyHunter <Nando.goat@gmail.com>
2023-05-25 15:51:12 +0200

    Created the unit conversion

--- src/UnitConversion.js
+++ src/UnitConversion.js
@@ -3,15 +3,40 @@ import "./styles.css"
 
 export default function UnitConversion(props) {
 const [unit, setUnit] = useState ("celsius")
+
+function showFarhenheit(event) {
+    event.preventDefault();
+    setUnit("farenheit")
+}
+function showCelsius(event) {
+    event.preventDefault();
+    setUnit("celsius")
+}
+
 if (unit === `celsius`) {
-
-
   return (
-    <div>
-      <span id="cityTemp">{Math.round(props.celsius)}</span>° <span className="unit" >C</span>
+    <div className="WeatherTemperature">
+      <span id="cityTemp">{Math.round(props.celsius)}</span>°  
+       <span className="unit" > 
+       C | 
+       <a href="/" onClick={showFarhenheit}> F </a> 
+       </span>
     </div>
   );
 } else {
-    return "F"
+    let farenheitTemp = (props.celsius * 9/5) + 32;
+
+   return (
+    <div className="WeatherTemperature">
+      <span id="cityTemp">{Math.round(farenheitTemp)}</span>° 
+
+       <span className="unit" > 
+       <a href="/" onClick={showCelsius}>C </a>
+    | F
+       </span>
+    </div>
+  );}
+  
 }
-}
+
+
